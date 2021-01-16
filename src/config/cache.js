938138687f08ad50ffd7dba0b1dfdc6d06d0eb9c
Dayvson Sales <dayvson@outlook.com>
2021-01-16 19:08:09 -0300

    Add helpers files
    Signed-off-by: Dayvson Sales <dayvson@outlook.com>

---
+++ src/config/cache.js
@@ -0,0 +1,7 @@
+module.exports = {
+  redis: {
+    host: process.env.REDIS_HOST || 'http://localhost',
+    port: Number(process.env.REDIS_PORT) || 6379,
+  },
+  resetTime: process.env.CACHE_RESET_TIME || 3600,
+};
---
+++ src/logger/index.js
@@ -0,0 +1,7 @@
+const log4js = require('log4js');
+
+const logger = log4js.getLogger('products');
+
+logger.level = 'error';
+
+module.exports = logger;
